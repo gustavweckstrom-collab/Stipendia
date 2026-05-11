066db9c555a79e5438cc87a2ae33f0a26d62d5c4
Wexenia <86799889+Wexenia@users.noreply.github.com>
2026-05-11 11:09:17 +0200

    Update scholarships.ts
    Added the possibility of a English description

--- src/data/scholarships.ts
+++ src/data/scholarships.ts
@@ -15,6 +15,7 @@ export interface Scholarship {
   name: string;
   organization: string;
   description: string;
+  descriptionEn?: string;
   targetGroup: string[];
   requirements: string[];
   location: string | null;
